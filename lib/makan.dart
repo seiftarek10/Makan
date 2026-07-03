9c8121fc44657bf1fcc71d84d0c31f0152770508
Seif Tarek <seift470@gmail.com>
2026-07-03 16:32:47 +0300

    apply localization with getx

--- lib/makan.dart
+++ lib/makan.dart
@@ -1,27 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:makan/core/app_router/router.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:makan/core/translation/app_translation.dart';
+
 class MakanApp extends StatelessWidget {
   const MakanApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-      return ScreenUtilInit(
+    return ScreenUtilInit(
       designSize: const Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-      // Use builder only if you need to use library outside ScreenUtilInit context
-      builder: (_ , child) {
-        return MaterialApp.router(
-      title: 'Makan',
-      debugShowCheckedModeBanner: false,
-      routerConfig: router,
-      
-    );
+      builder: (_, child) {
+        return GetMaterialApp(
+          translations: AppTranslations(), 
+          locale: const Locale('en', 'US'), 
+          fallbackLocale: const Locale(
+            'en',
+            'US',
+          ), 
+          title: 'Makan',
+          debugShowCheckedModeBanner: false,
+        );
       },
     );
-   
   }
-
 }
